5ad33cb46e5188ce1fffdfea6177df4dd14f78df
isayme <isaymeorg@gmail.com>
2023-11-09 22:13:15 +0800

    update

--- src/qinglong-checkin.ts
+++ src/qinglong-checkin.ts
@@ -1,7 +1,7 @@
 import { refreshToken } from './adrive'
 import { checkin } from './checkin'
 import { notifyDingtalk } from './dingtalk'
-import { persistRefreshToken } from './qinglong-persist-refresh-token'
+import { persistRefreshToken } from './persist-refresh-token'
 import { runMain } from './util'
 
 async function main() {
