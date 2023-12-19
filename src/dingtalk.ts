97e27a1b8c3192fab5b4c533803ada2e964f0885
isayme <isaymeorg@gmail.com>
2023-12-19 22:25:39 +0800

    update

--- src/dingtalk.ts
+++ src/dingtalk.ts
@@ -3,6 +3,9 @@ import logger from './logger'
 
 let robot = new Robot({
   url: process.env.DINGTALK_WEBHOOK_URL,
+  accessToken: process.env.DINGTALK_WEBHOOK_ACCESS_TOKEN,
+  secret: process.env.DINGTALK_WEBHOOK_SECRET,
+  timeout: 3000,
 })
 
 export async function notifyDingtalk(message: string) {
